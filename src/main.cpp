a4853e3425c3dfc6ae6a2c668e6614e9e00ac6da
codewitch-honey-crisis <gaztoast@gmail.com>
2024-06-16 14:56:26 -0700

    removed idf stuff

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,5 @@
-#if __has_include(<Arduino.h>)
 #include <Arduino.h>
-#else
-#include <freertos/FreeRTOS.h>
-#include <stdint.h>
-uint32_t millis() {
-    return pdTICKS_TO_MS(xTaskGetTickCount());
-}
-void loop();
-#endif
-#include <esp_i2c.hpp> // i2c initialization
-#ifdef M5STACK_CORE2
 #include <m5core2_power.hpp> // AXP192 power management (core2)
-#endif
-#ifdef M5STACK_TOUGH
-#include <m5tough_power.hpp> // AXP192 power management (tough)
-#endif
 #include <uix.hpp> // user interface library
 #include <gfx.hpp> // graphics library
 #include <mpu6886.hpp> // motion
@@ -25,48 +10,22 @@ void loop();
 #include "ui.hpp" // ui declarations
 #include "panel.hpp" // display panel functionality
 
-// namespace imports
-#ifdef ARDUINO
-using namespace arduino; // libs (arduino)
-#else
-using namespace esp_idf; // libs (idf)
-#endif
 using namespace gfx; // graphics
 using namespace uix; // user interface
 
-#ifdef M5STACK_CORE2
 using power_t = m5core2_power;
-#endif
-#ifdef M5STACK_TOUGH
-using power_t = m5tough_power;
-#endif
 // for AXP192 power management
-static power_t power(esp_i2c<1,21,22>::instance);
+static power_t power(Wire1);
 
 
 // the screen/control definitions
 screen_t main_screen;
 gyro_box_t main_cube(main_screen);
-mpu6886 gyro (esp_i2c<1,21,22>::instance);
+mpu6886 gyro (Wire1);
 
-#ifdef ARDUINO
 void setup() {
     Serial.begin(115200);
-    printf("Arduino version: %d.%d.%d\n",ESP_ARDUINO_VERSION_MAJOR,ESP_ARDUINO_VERSION_MINOR,ESP_ARDUINO_VERSION_PATCH);
-#else
-void loop_task(void* arg) {
-    int count;
-    while(1) {
-        count = 10;
-        while(count--) {
-            loop();
-        }
-        vTaskDelay(1);
-    }
-}
-extern "C" void app_main() {
-    printf("ESP-IDF version: %d.%d.%d\n",ESP_IDF_VERSION_MAJOR,ESP_IDF_VERSION_MINOR,ESP_IDF_VERSION_PATCH);
-#endif
+    Serial.printf("Arduino version: %d.%d.%d\n",ESP_ARDUINO_VERSION_MAJOR,ESP_ARDUINO_VERSION_MINOR,ESP_ARDUINO_VERSION_PATCH);
     power.initialize(); // do this first
     panel_init(); // do this next
     gyro.initialize();
@@ -84,11 +43,6 @@ extern "C" void app_main() {
     main_screen.register_control(main_cube);
     
     panel_set_active_screen(main_screen);
-
-#ifndef ARDUINO
-    TaskHandle_t handle;
-    xTaskCreatePinnedToCore(loop_task,"loop_task",4096,nullptr,24,&handle,xTaskGetAffinity(xTaskGetCurrentTaskHandle()));
-#endif
 }
 
 void loop()
@@ -110,9 +64,9 @@ void loop()
     ++frames;
     if(millis()>=time_ts+1000) {
         if(frames==0) {
-            printf("<1 FPS, Total: %dms\n",(int)total_ms);
+            Serial.printf("<1 FPS, Total: %dms\n",(int)total_ms);
         } else {
-            printf("%d FPS, Avg: %dms\n",frames,(int)(total_ms/frames));
+            Serial.printf("%d FPS, Avg: %dms\n",frames,(int)(total_ms/frames));
         }
         frames = 0;
         total_ms = 0;
